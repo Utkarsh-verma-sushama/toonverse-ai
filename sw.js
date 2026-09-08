56ae240ee9bd7527ad772fba3b17cc6a8138f3dc
Utkarsh-verma-sushama <uv7398@gmail.com>
2026-09-08 11:48:55 +0530

    Cache secure cloud sync client offline

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 "use strict";
-const CACHE_NAME = "toonverse-shell-v2";
+const CACHE_NAME = "toonverse-shell-v3";
 const CORE = [
   "./","./index.html","./create.html","./editor.html","./library.html","./404.html",
   "./assets/css/main.css","./assets/css/components.css","./assets/js/config.js","./assets/js/main.js",
-  "./assets/js/project-store.js",
+  "./assets/js/project-store.js","./assets/js/cloud-sync.js",
   "./manifest.webmanifest","./assets/toonverse-icon.svg"
 ];
 self.addEventListener("install", event => {
